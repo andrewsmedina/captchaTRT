f6aff2af00e35d0f8de807a510618b42543f3bc8
Julio Trecenti <julio.trecenti@gmail.com>
2017-04-14 21:23:33 -0300

    algumas funcoes de modelo

--- R/oraculo.R
+++ R/oraculo.R
@@ -62,6 +62,44 @@ get_state <- function(r) {
     rvest::html_attr('value')
 }
 
+max_freq <- function(x) {
+  tab <- table(x)
+  names(tab)[which.max(tab)]
+}
+
 predict_model <- function(img, model = NULL) {
-  '123456'
+  d <- captcha_trainset(img)
+  dd <- captcha_trainset_one(d$data[[1]], 'a')
+  dd$y <- as.factor(dd$y)
+  dd$id_captcha <- rep(d$captcha_id, each = 6 * 8)
+  dd$id_corte <- rep(rep(1:8, each = 6), nrow(d))
+  dd$id_letra <- rep(rep(1:6, 8), nrow(d))
+  dd$pred <- as.character(predict(model, dd))
+  lvs <- c(".", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "d",
+           "e", "f", "h", "j", "k", "m", "n", "r", "s", "t", "u", "v", "w",
+           "x", "y")
+  dd_ensemble <- dd %>%
+    dplyr::select(pred, x = id_corte, id_captcha, id_letra) %>%
+    tidyr::spread(x, pred, fill = '.', sep = '') %>%
+    dplyr::mutate_if(is.character, dplyr::funs(factor(., levels = lvs))) %>%
+    dplyr::select(x1:x8, id_captcha, id_letra)
+  preds <- as.character(predict(model_ensemble, dd_ensemble))
+  paste(preds, collapse = '')
+  # tibble::tibble(pred = preds, ordem = rep(1:6, 8)) %>%
+  #   dplyr::group_by(ordem) %>%
+  #   dplyr::summarise(l = max_freq(pred)) %>%
+  #   with(l) %>%
+  #   paste(collapse = '')
+  ##
+  # tibble::tibble(pred = preds, ordem = rep(1:6, 8), .id = rep(1:8, each = 6)) %>%
+  #   tidyr::spread(.id, pred) %>%
+  #   janitor::clean_names() %>%
+  #   dplyr::select(-ordem)
+}
+
+afff <- function() {
+  arqs <- dir('~/decryptr/captchaTRTData/inst/img', f=T)
+  arq <- sample(arqs, 1)
+  plot(magick::image_read(arq))
+  predict_model(arq, model)
 }
